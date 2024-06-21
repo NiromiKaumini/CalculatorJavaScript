063df77531252bcd5646efa563a6da32a6d320d8
Yasheena Niromi <jyniromikaumini123@gmail.com>
2024-06-22 01:19:45 +0530

    calculater

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const display = document.getElementById("display");
-function appendToDisplay(input){
+
+function appendToDisplay(input) {
     display.value += input;
 }
 
-function clearDisplay(){
-    display.value ="";
+function clearDisplay() {
+    display.value = "";
 }
 
-function calculate(){
-    display.value =eval(1+2+3);
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
 }
